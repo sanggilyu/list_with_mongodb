a2106e73103c2c828e8f7a86a06fb3d63474b42c
sanggilyu <sangjidev@gmail.com>
2024-10-16 21:08:09 +0900

    domain changed

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -1,18 +1,42 @@
+// src/api/products.js
+// import axios from "axios";
+
+// export const getProducts = async () => {
+//   const response = await axios.get("http://localhost:5000/products");
+//   return response.data;
+// };
+
+// export const addProduct = async (product) => {
+//   const response = await axios.post("http://localhost:5000/products", product);
+//   return response.data;
+// };
+
+// export const updateProduct = async (product) => {
+//   const response = await axios.put(
+//     `http://localhost:5000/products/${product._id}`,
+//     product
+//   );
+//   return response.data;
+// };
+
+// src/api/products.js
 import axios from "axios";
 
+const BASE_URL = "http://118.221.25.209:5000"; // 외부에서 접근 가능한 서버 주소
+
 export const getProducts = async () => {
-  const response = await axios.get("http://localhost:5000/products");
+  const response = await axios.get(`${BASE_URL}/products`);
   return response.data;
 };
 
 export const addProduct = async (product) => {
-  const response = await axios.post("http://localhost:5000/products", product);
+  const response = await axios.post(`${BASE_URL}/products`, product);
   return response.data;
 };
 
 export const updateProduct = async (product) => {
   const response = await axios.put(
-    `http://localhost:5000/products/${product._id}`,
+    `${BASE_URL}/products/${product._id}`,
     product
   );
   return response.data;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 서버 동작 시키기
+server 폴더: node index
+client 폴더: npm run start
+
+
+# 외부에서 앱 동작 시키기
+client/src/api/products.js에서 server 경로를 바꾸면 됨
+
+1. wifi 환경에서 동작시킬 때
+http://localhost:5000/products
+
+2. LTE 환경에서 동작시킬 때
+http://118.221.25.209:5000
